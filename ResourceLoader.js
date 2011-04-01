2ca609020768725bc49045a9e858a500f880e472
Rob Ashton <robashton@codeofrob.com>
2011-04-01 17:28:23 +0200

    Added buffer management to the resource loader

--- ResourceLoader.js
+++ ResourceLoader.js
@@ -2,7 +2,9 @@ var blah = blah || {};
 
 blah.ResourceLoader = function(){
   this._textureProviders = [];
+  this._bufferProviders = [];
   this._textureCache = {};
+  this._bufferCache = {};
   this._pendingTextureCount = 0;
 };
 
@@ -10,8 +12,28 @@ blah.ResourceLoader.prototype.addTextureProvider = function(provider) {
     this._textureProviders.push(provider);    
 };
 
+blah.ResourceLoader.prototype.addBufferProvider = function(provider) {
+    this._bufferProviders.push(provider);    
+};
+
+
 blah.ResourceLoader.prototype.getPendingTextureCount = function(){
     return this._pendingTextureCount;
+};
+
+blah.ResourceLoader.prototype.getBuffer = function(name, callback) {
+   var cachedBuffer = this._bufferCache[name];
+  if(cachedBuffer) {  callback(cachedBuffer); return; }
+  
+  for(var i in this._bufferProviders){
+   var provider = this._bufferProviders[i];
+   if(!provider.handles(name)){ continue; }
+   
+   var loadedBuffer = provider.load(name);
+   this._bufferCache[name] = loadedBuffer;
+   callback(loadedBuffer);
+   return;
+  }  
 };
 
 blah.ResourceLoader.prototype.getTexture = function(name) {
--- Tests/ResourceLoaderTests.js
+++ Tests/ResourceLoaderTests.js
@@ -56,9 +56,54 @@ $(document).ready(function(){
         loadedCallback();
     
         numberOfTexturesPending = loader.getPendingTextureCount();
-        equal(numberOfTexturesPending, 0, "Textures pending is 0 after a single texture is loaded");
+        equal(numberOfTexturesPending, 0, "Textures pending is 0 after a single texture is loaded");        
+    });
+    
+    test("Resource loader can retrieve buffers from providers", function(){
+        var loader = new blah.ResourceLoader();
+        var buffer = {};
+        var bufferProvider = {
+            handles: function(name) { return true; },
+            load: function(name) {            
+                return buffer;
+            }
+        };
+        loader.addBufferProvider(bufferProvider);        
+        var returnedBuffer = null;
         
+        loader.getBuffer("texture-buffer", function(buffer){
+            returnedBuffer = buffer;
+        });
+        
+        ok(buffer === returnedBuffer);
+    });
+    
+    test("Resource loader caches buffers if they have been loaded once", function() {
+        var loader = new blah.ResourceLoader();
+        var buffer = {};
+        var callCount = 0;
+        var bufferProvider = {
+            handles: function(name) { return true; },
+            load: function(name) {  
+                callCount++;
+                return buffer;
+            }
+        };
+        loader.addBufferProvider(bufferProvider);        
+        var returnedBuffer1= null;
+        var returnedBuffer2 = {};
+        
+        loader.getBuffer("texture-buffer", function(buffer){
+            returnedBuffer1 = buffer;
+        });
+        loader.getBuffer("texture-buffer", function(buffer){
+            returnedBuffer2 = buffer; 
+        });
+        
+        ok(returnedBuffer1 === returnedBuffer2, "Subsequent calls returned the same buffer");
+        equal(1, callCount, "Load was only called once for duplicate buffer");
         
     });
-
+    
+    
 });
