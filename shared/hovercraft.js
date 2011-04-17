a7f61a12304f07649757e3d3d90054905cb0cf20
Rob Ashton <robashton@codeofrob.com>
2011-04-17 16:21:52 +0200

    Craft slowed down so we can actually find each other

--- shared/hovercraft.js
+++ shared/hovercraft.js
@@ -5,14 +5,14 @@ var Hovercraft = {
     _velocity: vec3.create([0.01,0,0.01]),
     _decay: 0.97,
     impulseForward: function() {
-        var amount = 0.2;
+        var amount = 0.08;
         var accelerationZ = (-amount) * Math.cos(this.rotationY);
         var accelerationX = (-amount) * Math.sin(this.rotationY);
         var acceleration = vec3.create([accelerationX, 0, accelerationZ]);
         vec3.add(this._velocity, acceleration);
     },
     impulseBackward: function() {
-        var amount = 0.1;
+        var amount = 0.05;
         var accelerationZ = (amount) * Math.cos(this.rotationY);
         var accelerationX = (amount) * Math.sin(this.rotationY);
         var acceleration = vec3.create([accelerationX, 0, accelerationZ]);
@@ -27,7 +27,7 @@ var Hovercraft = {
         this.rotationY -= amount;
     },
     impulseUp: function(amount) {
-        var amount = 1.0;
+        var amount = 0.25;
         var terrain = this._scene.getEntity("terrain");
         
         var terrainHeight = terrain.getHeightAt(this.position[0], this.position[2]);
@@ -49,9 +49,9 @@ var Hovercraft = {
         }
          
          if(heightDelta < 10.0){
-               this._velocity[1] += (10.0 - heightDelta) * 0.08;
+               this._velocity[1] += (10.0 - heightDelta) * 0.03;
          }
-         this._velocity[1] -= 0.07;              
+         this._velocity[1] -= 0.025;              
          vec3.scale(this._velocity, this._decay);    
          
     }
