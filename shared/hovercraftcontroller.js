dbc9d7014f4c431085dd8a8db9f21ceb2dd5e5bb
Rob Ashton <robashton@codeofrob.com>
2011-05-08 20:23:20 +0200

    Removed the firing button for stability purposes

--- shared/hovercraftcontroller.js
+++ shared/hovercraftcontroller.js
@@ -40,12 +40,13 @@ HovercraftController.prototype.processInput = function(){
         this.server.sendMessage('message', { method: 'impulseUp' });
     }
     
+    /*
     if(KeyboardStates[KeyCodes.RCTRL]) {
        if(this.entity.canFire())
        {
             this.server.sendMessage('request_fire', {});
        }
-    }
+    } */
 };
 
 document.onkeydown = function(event) { 
