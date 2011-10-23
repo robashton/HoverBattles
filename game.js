5662b83fdbe11ba0e86653b0edaeccc001f513bc
Rob Ashton <robashton@codeofrob.com>
2011-10-23 19:52:59 +0200

    Half decent chase cam, (hah, decent - well by my standards)

--- game.js
+++ game.js
@@ -294,11 +294,21 @@ var ChaseCamera = {
   cameraMode: "chase",
   entity: null,
 
+  _ctor: function() {
+      this.cameraLocation = vec3.create([0,100,0]);
+      this.cameraVelocity = vec3.create([0,0,0]);
+      this.targetVelocity = vec3.create([0,0,0]);
+      
+  },
+
   setTrackedEntity: function(entity) {
     this.entity = entity;
+    this.cameraLocation = vec3.create(this.entity.position);
+    this.cameraLocation[1] = 10;
   },
 
   doLogic: function(){      
+    this._scene.camera.location = vec3.create(this.cameraLocation);
     if(this.cameraMode === "chase")
       this.doLogicForChaseCamera();
     else if(this.cameraMode === "death")
@@ -315,38 +325,53 @@ var ChaseCamera = {
 
      cameraTrail[1] = 0;
      vec3.normalize(cameraTrail);
-     vec3.scale(cameraTrail, 50);
+     vec3.scale(cameraTrail, 30);
      vec3.subtract(this.entity.position, cameraTrail, cameraTrail);
 
-     this._scene.camera.location = cameraTrail;
-     
+     var desiredCameraLocation = cameraTrail;
+
      var terrainHeightAtCameraLocation = terrain == null ? 10 : terrain.getHeightAt(this._scene.camera.location[0], 
                                                              this._scene.camera.location[2]);
                             
      var cameraHeight = Math.max(terrainHeightAtCameraLocation + 15, this.entity.position[1] + 15);
      
-     this._scene.camera.location[1] =  cameraHeight;
-
-     // Note: This should really be integrated with the above logic
-     this.cameraLocation = vec3.create(this._scene.camera.location);
+     desiredCameraLocation[1] =  cameraHeight;
+  
+     this.destinationCameraLocation = desiredCameraLocation;
+     this.targetVelocity = vec3.create(this.entity._velocity);
+     this.doLogicAfterAscertainingTarget();
   },
 
   doLogicForDeathCamera: function() {   
+    this.doLogicAfterAscertainingTarget();
+  },
+
+  doLogicAfterAscertainingTarget: function() {
+
+    // Our target has a velocity
+    // We have a location that is relative to that of the object being chased
+    // Our ideal position is another location that is relative to the object being chased
+    // We should aim to cover x% of that distance every frame?
+    // If we reach < y distance, we should look at the velocity of the object being chased
+    // And match that for our next movement so we move with the object
+    // If the velocity < z, then we should just stop
+
     var directionToWhereWeWantToBe = vec3.create();
     vec3.subtract(this.destinationCameraLocation, this.cameraLocation, directionToWhereWeWantToBe);
-    vec3.normalize(directionToWhereWeWantToBe);
+    var distance = vec3.length(directionToWhereWeWantToBe);
+    var movementTowardsDestination = 0.1;
 
-    vec3.scale(directionToWhereWeWantToBe, 0.1);
-    vec3.add(this.cameraVelocity, directionToWhereWeWantToBe);    
-    vec3.add(this._scene.camera.location, this.cameraVelocity);
+    vec3.scale(directionToWhereWeWantToBe, movementTowardsDestination, this.cameraVelocity);
+
+    vec3.add(this.cameraLocation, this.cameraVelocity); 
+    this._scene.camera.location = new vec3.create(this.cameraLocation);
   },
 
   startZoomingOutChaseCamera: function() {
     this.cameraMode = "death";
     this.destinationCameraLocation = vec3.create(this.entity.position);
     this.destinationCameraLocation[1] = 300.0;
-    this.playerDeathLocation = this.entity.position;
-    this.cameraVelocity = vec3.create([0,0,0]);
+    this.targetVelocity = vec3.create([0,0,0]);
   },
 
   startZoomingBackInChaseCamera: function() {
